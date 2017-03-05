530062e60497d438c8aca05077341dc6fc4f4b30
SAPIENT\pgoe12 <pgoel12@sapient.com>
2017-03-06 02:23:38 +0530

    Cleanup

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
   @Input() navList: {};
   constructor() {
   }
-
-  ngOnInit() {
-  }
-
 }
