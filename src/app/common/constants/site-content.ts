d306f7912c0bd85da843972f5f98c560968ece61
SAPIENT\uprata <upratap@sapient.com>
2017-03-08 10:40:36 +0530

    ts file in output comm

--- src/app/common/constants/site-content.ts
+++ src/app/common/constants/site-content.ts
@@ -4,6 +4,11 @@ export const SiteContent ={
             mainContent:'We have a module named "components" in our app. That module contains several components',
             componentOneDes:'File content of "Parent" component',
             componentTwoDes:'File content of "Child" component'
+        },
+        communicationOutput:{
+            mainContent:'We have Communication-output component as a parent component and output-child component as child component.Given below is the ts file of parent component',
+            componentOneDes:'File content of "Parent" component',
+            componentTwoDes:'File content of "Child" component'
         }
     }
 }
--- src/app/views/components/communication-output/communication-output.component.ts
+++ src/app/views/components/communication-output/communication-output.component.ts
@@ -3,15 +3,15 @@ import {FileReadService } from "../../../shared/services/file-read.service";
 import { SiteContent } from "../../../common/constants/site-content";
 import { ROUTES } from "../../../common/constants/routes"
 
-// start
+
 @Component({
   selector: 'app-communication-output',
   templateUrl: './communication-output.component.html',
   styleUrls: ['./communication-output.component.scss']
 })
 export class CommunicationOutputComponent implements OnInit {
-  // end
-
+ 
+// start
   easy = 0;
   mediocre = 0;
   difficult = 0;
@@ -21,10 +21,11 @@ export class CommunicationOutputComponent implements OnInit {
     if(value=="mediocre") this.mediocre++;
     if(value=="difficult") this.difficult++;
   }
+ // end
   
-  mainContent = SiteContent.components.multiple.mainContent;
-  componentOneDes = SiteContent.components.multiple.componentOneDes;
-  componentTwoDes = SiteContent.components.multiple.componentTwoDes;
+  mainContent = SiteContent.components.communicationOutput.mainContent;
+  componentOneDes = SiteContent.components.communicationOutput.componentOneDes;
+  componentTwoDes = SiteContent.components.communicationOutput.componentTwoDes;
 
   moduleFileContent: string;
   parentFileContent: string;
@@ -33,7 +34,7 @@ export class CommunicationOutputComponent implements OnInit {
 
   }
   ngOnInit() {
-    this._fileReadService.getFileContents('src/app/views/components/components.module.ts').subscribe(moduleFileContent => this.moduleFileContent = this._fileReadService.trimContent(moduleFileContent,FileReadService.TS));
+    this._fileReadService.getFileContents('src/app/views/components/communication-output/communication-output.component.ts').subscribe(moduleFileContent => this.moduleFileContent = this._fileReadService.trimContent(moduleFileContent,FileReadService.TS));
     this._fileReadService.getFileContents('src/app/views/components/communication-output/communication-output.component.html').subscribe(parentFileContent => this.parentFileContent = this._fileReadService.trimContent(parentFileContent,FileReadService.HTML));
     this._fileReadService.getFileContents('src/app/views/components/communication-output/output-child.component.ts').subscribe(childFileContent => this.childFileContent = this._fileReadService.trimContent(childFileContent,FileReadService.TS));
 
